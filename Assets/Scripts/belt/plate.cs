6076c2ed654f523ebd7c5e0c7e0b93e4c9b17e59
Victor Szczepanski <acgs02@gmail.com>
2015-11-15 16:09:23 -0700

    Adding prefab, new sprites, and iTween package.

---
+++ Assets/Scripts/belt/plate.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class plate : MonoBehaviour {
+
+    void Start () {
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("belt_path"), "time", 10, "easetype", iTween.EaseType.linear)); //, "looptype", iTween.LoopType.loop
+    }
+
+    // Update is called once per frame
+    void Update () {
+    }
+
+}
