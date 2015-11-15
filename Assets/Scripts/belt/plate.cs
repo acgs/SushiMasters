90b6d0a7c5d1a7c1d1b8fcb0054794697b4d192e
Victor Szczepanski <acgs02@gmail.com>
2015-11-15 16:41:52 -0700

    Fixed plate creation and timing.

--- Assets/Scripts/belt/plate.cs
+++ Assets/Scripts/belt/plate.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class plate : MonoBehaviour {
 
     void Start () {
-        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("belt_path"), "time", 10, "easetype", iTween.EaseType.linear)); //, "looptype", iTween.LoopType.loop
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("belt_path"), "time", 10, "easetype", iTween.EaseType.linear, "looptype", iTween.LoopType.loop)); //
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/plate_builder.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class plate_builder : MonoBehaviour {
+    public float delay = 0.5f;
+    public float nextUsage;
+    public int numPlates = 40;
+    public float initial_xPos = 0f;
+    public float initial_yPos = 2.159578f;
+    public float initial_zPos = 0.7407658f;
+    public Transform plate_prefab;
+    // Use this for initialization
+    void Start () {
+        nextUsage = Time.time + delay;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (numPlates > 0 && Time.time > nextUsage){
+            //instantiate plate prefab and set its script to enabled
+            Transform plateClone = (Transform) Instantiate(plate_prefab, new Vector3(initial_xPos, initial_yPos, initial_zPos), Quaternion.identity);
+            plateClone.GetComponent<plate>().enabled = true;
+            numPlates--;
+            nextUsage = Time.time + delay;
+        }
+    }
+}
