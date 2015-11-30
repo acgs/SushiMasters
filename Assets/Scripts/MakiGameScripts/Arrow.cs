f916ec0cf05aeec5f8b518174e8c28f304937c71
Victor Szczepanski <acgs02@gmail.com>
2015-11-29 17:36:18 -0700

    Added random direction to maki game arrows. Swapped sprites for new assets. Separated arrows from sushi.png into their own files.

--- Assets/Scripts/MakiGameScripts/Arrow.cs
+++ Assets/Scripts/MakiGameScripts/Arrow.cs
@@ -2,10 +2,15 @@
 using System.Collections;
 
 public class Arrow : MonoBehaviour {
+    private SpriteRenderer spriteRenderer;
     public bool rightToLeft; //all arrows start off right to left.
     public MakiGame makiGame; //handle to the makigame so we don't have to look it up.
     public bool beaten; //if this arrow has been beaten this game.
     public bool clicked; //if this arrow is currently being swiped on
+
+    /*Set through unity*/
+    public Sprite leftToRightSprite;
+    public Sprite rightToLeftSprite;
 
     // Use this for initialization
     void Start () {
@@ -13,6 +18,10 @@ public class Arrow : MonoBehaviour {
         clicked = false;
         //makiGame = null;
         beaten = false;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        if(spriteRenderer.sprite == null){
+            spriteRenderer.sprite = leftToRightSprite;
+        }
     }
 
     public void setMakiGame(MakiGame game){
@@ -21,6 +30,12 @@ public class Arrow : MonoBehaviour {
 
     public void setRightToLeft(bool rightToLeft){
         this.rightToLeft = rightToLeft;
+        if(this.rightToLeft){
+            spriteRenderer.sprite = rightToLeftSprite;
+        }
+        else{
+            spriteRenderer.sprite = leftToRightSprite;
+        }
     }
 
     public void toggleRightToLeft(){
